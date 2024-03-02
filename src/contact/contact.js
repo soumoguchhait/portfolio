fa67a31382bc376a961964f475a06d7f25b80b5f
soumya guchhait <soumoguchhait8587@gmail.com>
2024-03-02 18:34:43 +0530

    final commit

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -4,7 +4,7 @@ const Contact=()=>{
     return(
         <>
         <div id="contact" className="contact-container">
-            <h2>Get In Touch</h2>
+            <h2>Get In</h2>
             <h3>Although I’m not currently looking for any new opportunities, my inbox is always open.
                  Whether you have a question or just want to say hi, I’ll try my best to get back to you!Say Hello</h3>
                  <button><a href="mailto:soumoguchhait8587@gmail.com ?subject=hello" >Say Hello!</a></button>
